725ef8323367f7331804df435dc014e1e83948aa
Elan Fraga <elanfraga@gmail.com>
2019-07-04 00:00:43 -0300

    ESLint, Prettier & EditorConfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,26 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module'
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "no-unused-vars": ["erro", { argsIgnoreParttern: "next" }]
+    //todos problemas q o prettier indentificar apresentar como erro
+    'prettier/prettier': 'error',
+    //não precisa usar o this
+    'class-methods-use-this': 'off',
+    //receba um parametro e faça uma alteração no parametro utilizado no sequelize
+    'no-param-reassign': 'off',
+    //não precisar usar o formato da variável nomeVariavel
+    camelcase: 'off',
+    //variável que não é utilizada, ignorar o next
+    'no-unused-vars': ['error', { argsIgnoreParttern: 'next' }]
   }
 };
