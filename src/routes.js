be51033ea2db170860fb051f93e67c35f53cebbd
Elan Fraga <elanfraga@gmail.com>
2019-07-04 22:48:11 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'digo@rocketseta.com.br',
-    password_hash: '12345678',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
