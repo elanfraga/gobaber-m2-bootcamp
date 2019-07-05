0b8918c97684d0883553572e4aec9ff4301cdaa6
Elan Fraga <elanfraga@gmail.com>
2019-07-05 00:43:02 -0300

    Update do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,9 +19,30 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldpassword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email != user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ erro: 'User already exists.' });
+      }
+    }
+
+    if (oldpassword && !(await user.checkPassword(oldpassword))) {
+      return res.status(401).json({ error: 'Passord does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
