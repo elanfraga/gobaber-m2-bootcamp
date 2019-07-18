e0c7bf3238ee1eaf23fe86197e5b07f77c483d2c
Elan Fraga <elanfraga@gmail.com>
2019-07-18 01:06:32 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1a8db8d2d32400',
+    pass: '2418d3a09eb27f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
